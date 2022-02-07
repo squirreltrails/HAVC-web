b87394365e6504e0ec9b60010686e8eaf3bbabd9
squirreltrails <havancourt@gmail.com>
2022-02-06 19:39:50 -0600

    fixed linkage

--- React/my-react-app/src/App.js
+++ React/my-react-app/src/App.js
@@ -1,9 +1,10 @@
 
 import './App.css';
-import Counter from './components/counter';
+
+import Main from './components/main';
 function App() {
   return (
-    <Counter />
+    <Main />
   );
 }
 
---
+++ React/my-react-app/src/components/main.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div>
+           <ol>
+                  <li>Risk Concepts</li>
+                   <li>IT Audit</li>
+                   <li>Security and Breachesg/pong  </li>
+                    <li><a  href="https://spectrum.ieee.org/pong"  >
+           
+        Odd things that have come up, 
+                   </a> </li>
+    </ol>
+        </div>
+      );
+}
+ 
+export default Main;
